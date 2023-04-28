1b0bf90ec89be2a472b77799f1344e3f1e5a7b48
josdaperales <josdaperales@gmail.com>
2023-04-27 20:13:48 -0500

    responsive

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
     arrows: true,
     autoplay: true, // Habilitar reproducción automática
     interval: 3000, // Intervalo de tiempo en milisegundos entre cada transición
-    transition: 'fade'
+    transition: 'fade',
   }).mount();
 });
