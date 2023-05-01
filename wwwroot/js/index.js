a220e40c9fe2e94cb7d80d1e249e4c372638193f
josdaperales <josdaperales@gmail.com>
2023-05-01 17:08:39 -0500

    footer

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-  new Splide('#splide', {
-    type: 'loop',
-    perPage: 1,
-    arrows: true,
-    autoplay: true, // Habilitar reproducción automática
-    interval: 3000, // Intervalo de tiempo en milisegundos entre cada transición
-    transition: 'fade',
-  }).mount();
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true, // Habilita el loop
+    autoplay: {
+      delay: 3000, // Cambia el slide cada 3 segundos
+      disableOnInteraction: false, // Continúa el autoplay después de interactuar con el carrusel
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 });
