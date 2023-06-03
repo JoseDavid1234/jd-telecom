9796528edab21fcf6b307bd2519ce44324770ed1
JoseDavid1234 <josdaperales@gmail.com>
2023-06-03 17:51:21 -0500

    cobertura added

---
+++ Controllers/Cobertura.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JDTelecomunicaciones.Controllers
+{
+    public class Cobertura : Controller
+    {
+        private readonly ILogger<Cobertura> _logger;
+
+        public Cobertura(ILogger<Cobertura> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
